d8707c94a425ac638a4e01fc23129ab71a7862da
Binny V A <binnyva@gmail.com>
2024-10-16 22:41:48 +0100

    feat: Show Day name in calendar

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
 # Glocal
 
-- Show Month - with links to each day's journal entry
-- month view should have links to Week Page as well
+- Close button
+- Month view should have links to Week Page as well
+  - It should open week page on click
 - Show Option to go to year view
   - Show name of months with links to it
   - Show all week links
+  
+## Done
+
+- [x] Show Month - with links to each day's journal entry
+- [x] Show day title
--- src/components/calender.tsx
+++ src/components/calender.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   format,
   addMonths,
@@ -9,31 +9,36 @@ import {
   isSameMonth,
   isSameDay,
   startOfWeek,
-  endOfWeek
-} from 'date-fns';
-import { PageEntity } from '@logseq/libs/dist/LSPlugin';
+  endOfWeek,
+} from "date-fns";
+import { PageEntity } from "@logseq/libs/dist/LSPlugin";
 
 interface CalendarProps {
   initialDate?: Date;
 }
 
-async function getJournalEntriesFromTo(startDate: Date, endDate: Date): Promise<PageEntity[]> {  
+async function getJournalEntriesFromTo(
+  startDate: Date,
+  endDate: Date
+): Promise<PageEntity[]> {
   let pages;
   try {
     pages = await logseq.DB.datascriptQuery(`
       [:find (pull ?p [
-            :block/name :block/properties :block/journal-day :block/uuid :block/original-name
-          ])
-       :where
-       [?b :block/page ?p]
-       [?p :block/journal? true]
-       [?p :block/journal-day ?d]
-       [(>= ?d ${format(startDate, 'yyyyMMdd')})] [(<= ?d ${format(endDate,'yyyyMMdd')})]]
-    `)
+        :block/name :block/properties :block/journal-day :block/uuid :block/original-name
+        {:block/_page [:block/content]}])
+      :where
+      [?b :block/page ?p]
+      [?p :block/journal? true]
+      [?p :block/journal-day ?d]
+      [(>= ?d ${format(startDate, "yyyyMMdd")})] 
+      [(<= ?d ${format(endDate, "yyyyMMdd")})]
+    ]`);
+
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-  
+
   return pages;
 }
 
@@ -47,17 +52,19 @@ const Calendar: React.FC<CalendarProps> = ({ initialDate = new Date() }) => {
       const end = endOfWeek(endOfMonth(currentDate));
 
       const journalEntries = await getJournalEntriesFromTo(start, end);
-      setEntries(journalEntries);
+      if(journalEntries) {
+        setEntries(journalEntries);
+      }
     };
 
     fetchEntries();
   }, [currentDate]);
 
-  // useEffect(() => {
-  //   console.log(entries);
-  // }, [entries])
+  useEffect(() => {
+    console.log(entries);
+  }, [entries])
 
-  const weekDayLabels = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+  const weekDayLabels = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
   const prevMonth = (): void => {
     setCurrentDate(subMonths(currentDate, 1));
@@ -71,11 +78,11 @@ const Calendar: React.FC<CalendarProps> = ({ initialDate = new Date() }) => {
     const journal = entries.find(
       (journal) => journal[0]["journal-day"].toString() === day
     );
-    if(!journal) return;
+    if (!journal) return;
     // logseq.Editor.openInRightSidebar(journal[0].uuid); // May be if shift is pressed
-    logseq.App.pushState('page', { name: journal[0]['original-name'] })
+    logseq.App.pushState("page", { name: journal[0]["original-name"] });
     window.logseq.hideMainUI();
-  }
+  };
 
   const getDaysToDisplay = (): Date[] => {
     const start = startOfWeek(startOfMonth(currentDate));
@@ -88,22 +95,35 @@ const Calendar: React.FC<CalendarProps> = ({ initialDate = new Date() }) => {
 
     return (
       <div className="grid grid-cols-7 h-85">
-        {days.map((day, index) => (
-          <div
-            key={index}
-            className={`border border border-gray-600 ${
-              !isSameMonth(day, currentDate) ? 'text-gray-600 .opacity-40' : ''
-            }`}
-          >
-            <span className={`text-sm p-1
-              ${isSameDay(day, new Date()) ? 'bg-blue-500 text-white rounded-full' : ''}`}
-              onClick={() => openJournal(format(day, 'yyyyMMdd'))}
+        {days.map((day, index) => {
+          const journal = entries.find(
+            (journal) => journal[0]["journal-day"].toString() === format(day, 'yyyyMMdd')
+          );
+
+          return (
+            <div
+              key={index}
+              className={`border border border-gray-600 ${
+                !isSameMonth(day, currentDate)
+                  ? "text-gray-600 .opacity-40"
+                  : ""
+              }`}
             >
-              {format(day, 'd')}
-            </span>
-            <div>Title of {format(day, 'y m d')} goes here</div>
-          </div>
-        ))}
+              <span
+                className={`text-sm p-1
+              ${
+                isSameDay(day, new Date())
+                  ? "bg-blue-500 text-white rounded-full"
+                  : ""
+              }`}
+                onClick={() => openJournal(format(day, "yyyyMMdd"))}
+              >
+                {format(day, "d")}
+              </span>
+              <div>{journal ? journal[0].properties?.name : ""}</div>
+            </div>
+          );
+        })}
       </div>
     );
   };
@@ -111,19 +131,21 @@ const Calendar: React.FC<CalendarProps> = ({ initialDate = new Date() }) => {
   return (
     <div className="w-full h-full p-4 rounded shadow">
       <div className="flex justify-between items-center mb-4">
-      <button onClick={prevMonth}>
+        <button onClick={prevMonth}>
           <span className="h-4 w-4">&lt;</span>
         </button>
         <h2 className="text-lg font-semibold">
-          {format(currentDate, 'MMMM yyyy')}
+          {format(currentDate, "MMMM yyyy")}
         </h2>
         <button onClick={nextMonth}>
           <span className="h-4 w-4">&gt;</span>
         </button>
       </div>
       <div className="grid grid-cols-7 gap-1 text-center mb-2">
-        {weekDayLabels.map(day => (
-          <div key={day} className="font-medium text-sm">{day}</div>
+        {weekDayLabels.map((day) => (
+          <div key={day} className="font-medium text-sm">
+            {day}
+          </div>
         ))}
       </div>
       {renderCalendar()}
